df3c13894f0e13c8e40eaca41057c481e9cfcd7a
yurii-kovalchuk <sogdrums@gmail.com>
2023-07-02 21:20:14 +0300

    add styles to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import "./Home.style.css";
+
 export const Home = () => {
   return (
     <>
-      <div>Home</div>
-      <NavLink to="/tweets">Tweets</NavLink>
+      <div className="wrapHome">
+        <h1>Welcome to app</h1>
+        <h2>To see tweets follow the link</h2>
+        <NavLink to="/tweets" className="tweetsLink">
+          Tweets
+        </NavLink>
+      </div>
     </>
   );
 };
