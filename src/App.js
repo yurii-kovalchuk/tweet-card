0ccc3d0a30392a999d60ac7b86259ef3bf7c4089
yurii-kovalchuk <sogdrums@gmail.com>
2023-07-04 23:38:59 +0300

    solve problem with routes and gh-pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import { Home } from "./pages/Home/Home";
 import { Tweets } from "./pages/Tweets/Tweets";
+import { SharedLayout } from "./components/SharedLayout/SharedLayout";
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/tweets" element={<Tweets />} />
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="tweets" element={<Tweets />} />
+      </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
